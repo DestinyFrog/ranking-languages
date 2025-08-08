51b887047baf528ac1a1b38fbbdf2db82879d537
DestinyFrog <pedrocalizto9@gmail.com>
2025-08-08 17:11:36 -0300

    try drag and drop

--- script.js
+++ script.js
@@ -12,24 +12,53 @@
 
     const linguagens_options = document.getElementById('linguagens-options')
 
+    linguagens.forEach(({ name, path }) => {
+        const li = document.createElement('li')
+        li.className = 'rank-item'
+        implementDragAndDrop(li)
+
+        const img = document.createElement('img')
+        img.src = path
+        img.alt = `logo de ${name}`
+        img.title = capitalize(name)
+        li.appendChild(img)
+
+        linguagens_options.appendChild(li)
+    })
+
+    /**
+     * @param {HTMLElement} element 
+     */
+    function implementDragAndDrop(element) {
+        let offset = { x: 0, y: 0 }
+        
+        function mouseMove(ev) {
+            if (ev.clientX > 0 && ev.clientX < document.body.clientWidth) {
+                element.style.left = `${ev.clientX - offset.x}px`	
+            }
+
+            if (ev.clientY > 0 && ev.clientY < document.body.clientHeight) {
+                element.style.top = `${ev.clientY - offset.y}px`
+            }
+        }
+
+        function mouseDown(ev) {
+            offset.x = ev.clientX - element.offsetLeft
+            offset.y = ev.clientY - element.offsetTop
+
+            element.classList.add('draggable')
+
+            document.addEventListener('mousemove', mouseMove)
+            document.addEventListener('mouseup', _ =>
+                document.removeEventListener('mousemove', mouseMove))
+        }
+
+        element.addEventListener('mousedown', mouseDown)
+    }
+
     function capitalize(str) {
         if (!str || typeof str !== 'string') return '';
         return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
     }
-
-    function createLinguagemElement({ name, path }) {
-        const img = document.createElement('img')
-        img.className = 'rank-item'
-        img.src = path
-        img.alt = `logo de ${name}`
-        img.title = capitalize(name)
-        return img
-    }
-
-    linguagens.forEach(linguagem => {
-        const logo_linguagens = createLinguagemElement(linguagem)
-        linguagens_options.appendChild(logo_linguagens)
-    });
-
 })
 .call(this)
